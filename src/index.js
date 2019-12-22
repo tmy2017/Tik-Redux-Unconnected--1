8466603c93ec1912b362c0e67eeab6db0e72ca08
Tmy2017 <30006459+tmy2017@users.noreply.github.com>
2019-12-22 16:47:28 +0100

    actually reducer already in store.js!

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import Provider from "react-redux";
 
 import TodoApp from "./TodoApp";
+import store from "./redux/store";
 
 const rootElement = document.getElementById("root");
-ReactDOM.render(<TodoApp />, rootElement);
+ReactDOM.render(
+  <Provider store={store}>
+    <TodoApp />
+  </Provider>,
+  rootElement
+);
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,4 @@
 import { createStore } from "redux";
 import rootReducer from "./reducers";
-
+//zzCmt: so reducer already written la!
 export default createStore(rootReducer);
